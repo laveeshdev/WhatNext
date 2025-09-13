82d81a459e2f0679ccf84c29beeb458bc8cd8218
laveeshsanadhya <laveeshsanadhya@gmail.com>
2025-09-13 12:29:43 +0530

    cors update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json()); // For parsing application/json
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: [process.env.CORS_ORIGIN, 'http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 
